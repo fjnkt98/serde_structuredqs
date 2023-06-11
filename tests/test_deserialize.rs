acb7df602cdc2c79a0e817f03767b3ae4258e5c1
fjnkt98 <fjnkt98@gmail.com>
2023-06-11 11:00:15 +0900

    update dependencies

--- tests/test_deserialize.rs
+++ tests/test_deserialize.rs
@@ -1,57 +1,60 @@
-use serde::Deserialize;
+#[cfg(test)]
+mod test {
+    use serde_derive::Deserialize;
 
-#[test]
-fn test_deserialize_flat() {
-    #[derive(Debug, Deserialize, Eq, PartialEq)]
-    struct SearchParams {
-        keyword: Option<String>,
-        limit: Option<u32>,
+    #[test]
+    fn test_deserialize_flat() {
+        #[derive(Debug, Deserialize, Eq, PartialEq)]
+        struct SearchParams {
+            keyword: Option<String>,
+            limit: Option<u32>,
+        }
+
+        let expected = SearchParams {
+            keyword: Some(String::from("foo")),
+            limit: Some(20),
+        };
+
+        let source = "keyword=foo&limit=20";
+        let actual: SearchParams = serde_structuredqs::from_str(source).unwrap();
+        assert_eq!(actual, expected)
     }
 
-    let expected = SearchParams {
-        keyword: Some(String::from("foo")),
-        limit: Some(20),
-    };
+    #[test]
+    fn test_deserialize_structured() {
+        #[derive(Debug, Deserialize, Eq, PartialEq)]
+        struct SearchParams {
+            keyword: Option<String>,
+            limit: Option<u32>,
+            filter: Option<FilteringParameter>,
+        }
 
-    let source = "keyword=foo&limit=20";
-    let actual: SearchParams = serde_structuredqs::from_str(source).unwrap();
-    assert_eq!(actual, expected)
-}
+        #[derive(Debug, Deserialize, Eq, PartialEq)]
+        struct FilteringParameter {
+            category: Option<String>,
+            difficulty: Option<RangeFilteringParameter<i32>>,
+        }
 
-#[test]
-fn test_deserialize_structured() {
-    #[derive(Debug, Deserialize, Eq, PartialEq)]
-    struct SearchParams {
-        keyword: Option<String>,
-        limit: Option<u32>,
-        filter: Option<FilteringParameter>,
-    }
+        #[derive(Debug, Deserialize, Eq, PartialEq)]
+        struct RangeFilteringParameter<T> {
+            from: Option<T>,
+            to: Option<T>,
+        }
 
-    #[derive(Debug, Deserialize, Eq, PartialEq)]
-    struct FilteringParameter {
-        category: Option<String>,
-        difficulty: Option<RangeFilteringParameter<i32>>,
-    }
-
-    #[derive(Debug, Deserialize, Eq, PartialEq)]
-    struct RangeFilteringParameter<T> {
-        from: Option<T>,
-        to: Option<T>,
-    }
-
-    let expected = SearchParams {
-        keyword: Some(String::from("foo")),
-        limit: Some(20),
-        filter: Some(FilteringParameter {
-            category: Some(String::from("A")),
-            difficulty: Some(RangeFilteringParameter {
-                from: None,
-                to: Some(800),
+        let expected = SearchParams {
+            keyword: Some(String::from("foo")),
+            limit: Some(20),
+            filter: Some(FilteringParameter {
+                category: Some(String::from("A")),
+                difficulty: Some(RangeFilteringParameter {
+                    from: None,
+                    to: Some(800),
+                }),
             }),
-        }),
-    };
+        };
 
-    let source = "keyword=foo&limit=20&filter.category=A&filter.difficulty.to=800";
-    let actual: SearchParams = serde_structuredqs::from_str(source).unwrap();
-    assert_eq!(actual, expected)
+        let source = "keyword=foo&limit=20&filter.category=A&filter.difficulty.to=800";
+        let actual: SearchParams = serde_structuredqs::from_str(source).unwrap();
+        assert_eq!(actual, expected)
+    }
 }
